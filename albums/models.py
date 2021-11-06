ca8506b52da5aecd2db8e2a62cb2bd14a4bb4c96
James Moore <james816am@gmail.com>
2021-11-05 22:35:06 -0400

    Basic functionality in place

---
+++ albums/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255, blank=True, null=True)
+    # artist = models.ForeignKey(
+    #     'Album', on_delete=models.CASCADE, default=None,)
+    year_published = models.IntegerField(max_length=4, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
